76d7fce57a8dc51d51a05728a55fe5b8bd2dec24
Jochen Wersdörfer <jochen@wersdoerfer.de>
2021-10-13 10:30:41 +0200

    websocket wip

--- backend/views.py
+++ backend/views.py
@@ -1,6 +1,6 @@
-from typing import Optional
-
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+
+from .connection import connection_manager
 
 app = FastAPI()
 
@@ -12,11 +12,12 @@ def read_root():
 
 @app.websocket("/ws/{client_id}")
 async def websocket_endpoint(websocket: WebSocket, client_id: int):
-    await websocket.accept()
+    await connection_manager.connect(websocket)
     try:
         while True:
             data = await websocket.receive_json()
-            await websocket.send_json({"message": "message from backend!"})
+            await connection_manager.broadcast({"message": "message from backend!"})
             print("received data: ", data)
     except WebSocketDisconnect:
-        print("client has closed connection")
+        connection_manager.disconnect(websocket)
+        await connection_manager.broadcast(f"Client #{client_id} left the chat")
