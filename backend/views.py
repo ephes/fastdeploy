2da175bca3658fce08e230aa2447b80d741fc4e5
Jochen Wersdörfer <jochen@wersdoerfer.de>
2021-10-13 16:24:27 +0200

    added cors middleware

--- backend/views.py
+++ backend/views.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.middleware.cors import CORSMiddleware
 
 from .connection import connection_manager
 
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost",
+        "http://localhost:3000",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
