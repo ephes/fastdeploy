916f011c5b1bf7e3bd93c41e46e4578a25f3a2c5
Jochen Wersdörfer <jochen@wersdoerfer.de>
2021-10-14 14:34:42 +0200

    refactored websocket init

--- frontend/src/client.ts
+++ frontend/src/client.ts
@@ -7,17 +7,21 @@ class Client {
 
   constructor() {
     this.messages = ref([]);
+    this.initWebsocketConnection()
+  }
+
+  initWebsocketConnection() {
     this.connection = new WebSocket('ws://localhost:8000/ws/1');
-    this.connection.onopen = function (event: MessageEvent) {
+    this.connection.onopen = (event: MessageEvent) => {
       console.log(event);
       console.log('Successfully connected to the echo websocket server...');
-    };
-
+    }
     this.connection.onmessage = (event: MessageEvent) => {
       const message = JSON.parse(event.data);
       console.log('in client.ts: ', message);
       this.messages.value.push(message);
     };
+
   }
   sendMessage() {
     this.connection.send(
