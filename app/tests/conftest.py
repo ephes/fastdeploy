2760d147b6ce6f902ddca21b3e864d3073df3d61
Jochen Wersdörfer <jochen@wersdoerfer.de>
2021-10-18 07:43:00 +0200

    refresh explicitly

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,6 +1,6 @@
 import pytest
 
-from sqlmodel import Session, select
+from sqlmodel import Session
 
 from ..auth import get_password_hash
 from ..config import settings
@@ -20,14 +20,11 @@ def user(password):
 
 @pytest.fixture
 def user_in_db(password):
-    # user does not get refreshed - bug in sqlmodel? FIXME
     user = User(name="user", password=get_password_hash(password), is_active=True)
-    user_copy = user.copy()
     with Session(settings.db_engine) as session:
         session.add(user)
         session.commit()
-    with Session(settings.db_engine) as session:
-        user = session.exec(select(User).where(User.name == user_copy.name)).first()
+        session.refresh(user)
     return user
 
 
