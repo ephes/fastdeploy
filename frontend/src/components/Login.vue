abb56711821e1597c698ab5108a68fddb52cc33f
Jochen Wersdörfer <jochen@wersdoerfer.de>
2021-10-23 07:04:05 +0200

    emit login on pressing enter

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -10,7 +10,7 @@ defineEmits(['login']);
   <div>
     <h1>Login</h1>
     <input v-model="username" placeholder="foobar" />
-    <input v-model="password" type="password" />
+    <input v-model="password" type="password" @keyup.enter="$emit('login', username, password)"/>
     <button @click="$emit('login', username, password)">login</button>
     <br />
     {{ username }}
