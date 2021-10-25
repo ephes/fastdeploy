35ccfffc0d3ea145ab475201b07f7e282d3f7945
Jochen Wersdörfer <jochen@wersdoerfer.de>
2021-10-25 11:32:05 +0200

    added service, deployment, task models + auto created timestamp (utc)

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
+from datetime import datetime, timezone
 from typing import Optional
 
-from sqlalchemy import String
+from sqlalchemy import DateTime, String
 from sqlalchemy.sql.schema import Column
 from sqlmodel import Field, SQLModel
 
@@ -12,5 +13,24 @@ class User(SQLModel, table=True):
     deploy_only: bool
 
 
+class Service(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str = Field(sa_column=Column("name", String, unique=True))
+
+
 class Deployment(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
+    service_id: int = Field(foreign_key="service.id")
+    created: datetime = Field(
+        default=datetime.now(timezone.utc),
+        sa_column=Column("created", DateTime),
+    )
+
+
+class Task(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    deployment_id: int = Field(foreign_key="deployment.id")
+    created: datetime = Field(
+        default=datetime.now(timezone.utc),
+        sa_column=Column("created", DateTime),
+    )
