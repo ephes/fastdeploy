d2bc64ebb34963d2e20d1737cad361e7b669964c
Jochen Wersdörfer <jochen@wersdoerfer.de>
2021-10-28 10:40:06 +0200

    renamed task -> step

--- app/tests/test_routers_steps.py
+++ app/tests/test_routers_steps.py
@@ -4,10 +4,9 @@ from httpx import AsyncClient
 
 
 @pytest.mark.asyncio
-async def test_post_task_no_access_token(app, base_url, step):
+async def test_post_step_no_access_token(app, base_url, step):
     async with AsyncClient(app=app, base_url=base_url) as client:
         test_url = app.url_path_for("steps")
-        print("test_url: ", test_url)
         response = await client.post(test_url, data=step.dict())
 
     assert response.status_code == 401
