f24a9f5b639d6c1613c3f1824ec79cfdb1f1cad1
Jochen Wersdörfer <jochen@wersdoerfer.de>
2021-10-28 13:48:06 +0200

    json instead of data, data is for form data

--- app/tests/test_routers_steps.py
+++ app/tests/test_routers_steps.py
@@ -7,7 +7,7 @@ from httpx import AsyncClient
 async def test_post_step_no_access_token(app, base_url, step):
     async with AsyncClient(app=app, base_url=base_url) as client:
         test_url = app.url_path_for("steps")
-        response = await client.post(test_url, data=step.dict())
+        response = await client.post(test_url, json=step.dict())
 
     assert response.status_code == 401
     assert response.json() == {"detail": "Not authenticated"}
@@ -18,7 +18,7 @@ async def test_post_step_invalid_access_token(app, base_url, step, invalid_deplo
     headers = {"authorization": f"Bearer {invalid_deploy_token}"}
     async with AsyncClient(app=app, base_url=base_url) as client:
         test_url = app.url_path_for("steps")
-        response = await client.post(test_url, data=step.dict(), headers=headers)
+        response = await client.post(test_url, json=step.dict(), headers=headers)
 
     assert response.status_code == 401
     assert response.json() == {"detail": "Could not validate credentials"}
@@ -29,7 +29,7 @@ async def test_post_step_deployment_not_found(app, base_url, step, valid_deploy_
     headers = {"authorization": f"Bearer {valid_deploy_token}"}
     async with AsyncClient(app=app, base_url=base_url) as client:
         test_url = app.url_path_for("steps")
-        response = await client.post(test_url, data=step.dict(), headers=headers)
+        response = await client.post(test_url, json=step.dict(), headers=headers)
 
     assert response.status_code == 401
     assert response.json() == {"detail": "Could not validate credentials"}
@@ -44,4 +44,3 @@ async def test_post_step_deployment_found(app, base_url, step, valid_deploy_toke
 
     assert response.status_code == 200
     assert response.json() == {"received": True}
-    assert False
