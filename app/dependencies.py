16d401f425877221e9d09e53543fc6a2645a7f09
Jochen Wersdörfer <jochen@wersdoerfer.de>
2021-10-28 10:36:45 +0200

    obsolete

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,13 +1,11 @@
-from fastapi import Depends, HTTPException, status
-from sqlmodel import Session, select
+from fastapi import Depends
 
-from . import database
 from .auth import (
     get_current_deployment,
     get_current_service_and_origin,
     get_current_user,
 )
-from .models import Deployment, Service, ServiceAndOrigin, User
+from .models import Deployment, ServiceAndOrigin, User
 
 
 async def get_current_active_user(current_user: User = Depends(get_current_user)):
@@ -22,14 +20,3 @@ async def get_current_service_and_origin(
 
 async def get_current_active_deployment(deployment: Deployment = Depends(get_current_deployment)):
     return deployment
-
-
-def get_service_by_name(name: str) -> Service:
-    with Session(database.engine) as session:
-        service_from_db = session.exec(select(Service).where(Service.name == name)).first()
-    if service_from_db is None:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Service not found",
-        )
-    return service_from_db
