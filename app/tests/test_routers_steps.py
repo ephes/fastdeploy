3ccc9fbcd538422bdde07b77428bc9ed5ce36127
Jochen Wersdörfer <jochen@wersdoerfer.de>
2021-10-28 15:22:02 +0200

    refactoring (remove obsolete lines after installing black shortcut)

--- app/tests/test_routers_steps.py
+++ app/tests/test_routers_steps.py
@@ -6,8 +6,7 @@ from httpx import AsyncClient
 @pytest.mark.asyncio
 async def test_add_step_no_access_token(app, base_url, step):
     async with AsyncClient(app=app, base_url=base_url) as client:
-        test_url = app.url_path_for("steps")
-        response = await client.post(test_url, json=step.dict())
+        response = await client.post(app.url_path_for("steps"), json=step.dict())
 
     assert response.status_code == 401
     assert response.json() == {"detail": "Not authenticated"}
@@ -15,10 +14,10 @@ async def test_add_step_no_access_token(app, base_url, step):
 
 @pytest.mark.asyncio
 async def test_add_step_invalid_access_token(app, base_url, step, invalid_deploy_token):
-    headers = {"authorization": f"Bearer {invalid_deploy_token}"}
     async with AsyncClient(app=app, base_url=base_url) as client:
-        test_url = app.url_path_for("steps")
-        response = await client.post(test_url, json=step.dict(), headers=headers)
+        response = await client.post(
+            app.url_path_for("steps"), json=step.dict(), headers={"authorization": f"Bearer {invalid_deploy_token}"}
+        )
 
     assert response.status_code == 401
     assert response.json() == {"detail": "Could not validate credentials"}
@@ -26,10 +25,10 @@ async def test_add_step_invalid_access_token(app, base_url, step, invalid_deploy
 
 @pytest.mark.asyncio
 async def test_add_step_deployment_not_found(app, base_url, step, valid_deploy_token, cleanup_database_after_test):
-    headers = {"authorization": f"Bearer {valid_deploy_token}"}
     async with AsyncClient(app=app, base_url=base_url) as client:
-        test_url = app.url_path_for("steps")
-        response = await client.post(test_url, json=step.dict(), headers=headers)
+        response = await client.post(
+            app.url_path_for("steps"), json=step.dict(), headers={"authorization": f"Bearer {valid_deploy_token}"}
+        )
 
     assert response.status_code == 401
     assert response.json() == {"detail": "Could not validate credentials"}
@@ -37,10 +36,12 @@ async def test_add_step_deployment_not_found(app, base_url, step, valid_deploy_t
 
 @pytest.mark.asyncio
 async def test_add_step(app, base_url, step, valid_deploy_token_in_db):
-    headers = {"authorization": f"Bearer {valid_deploy_token_in_db}"}
     async with AsyncClient(app=app, base_url=base_url) as client:
-        test_url = app.url_path_for("steps")
-        response = await client.post(test_url, json=step.dict(), headers=headers)
+        response = await client.post(
+            app.url_path_for("steps"),
+            json=step.dict(),
+            headers={"authorization": f"Bearer {valid_deploy_token_in_db}"},
+        )
 
     assert response.status_code == 200
     assert response.json() == {"received": True}
