951cd94d67adea8bec68aa9b657bd933e0f3c2ec
Jochen Wersdörfer <jochen@wersdoerfer.de>
2021-11-02 20:36:02 +0100

    forgot name on insert

--- deployments/fastdeploy_collect.py
+++ deployments/fastdeploy_collect.py
@@ -12,6 +12,6 @@ playbook_path = Path("/Users/jochen/projects/fastdeploy/ansible/deploy.yml")
 with playbook_path.open("r") as f:
     parsed = yaml.safe_load(f)
 steps = parsed[0]["tasks"]
-steps.insert(0, "Gathering Facts")
+steps.insert(0, {"name": "Gathering Facts"})
 print(json.dumps(steps))
 # print("hello world: ", sys.executable)
