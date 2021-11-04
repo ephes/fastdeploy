45117fe84859078b61ef785615ab5f69a9123739
Jochen Wersdörfer <jochen@wersdoerfer.de>
2021-11-04 16:39:16 +0100

    rename services to ServiceList

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,14 +6,14 @@ import { createClient } from './client'
 import Login from './components/Login.vue'
 import HelloWorldVue from './components/HelloWorld.vue'
 import Home from './components/Home.vue'
-import Services from './components/Services.vue'
+import ServiceList from './components/ServiceList.vue'
 import DeploymentVue from './components/Deployment.vue'
 
 
 const client = createClient()
 
 const routes = [
-    { path: '/', component: Services },
+    { path: '/', component: ServiceList },
     { path: '/login', name: 'login', component: Login },
     { path: '/hello', component: HelloWorldVue },
     { path: '/deployment', component: DeploymentVue },
