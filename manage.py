c2177b0f721e7c115fb95c1d4fa519780226c519
Jochen Wersdörfer <jochen@wersdoerfer.de>
2021-11-07 14:33:21 +0100

    create service via application server

--- manage.py
+++ manage.py
@@ -1,13 +1,17 @@
 import os
 import subprocess
 
+from datetime import timedelta
+
 import typer
 
+from httpx import Client
 from rich import print as rprint
 from rich.prompt import Prompt
 
 from app import database
-from app.auth import get_password_hash
+from app.auth import create_access_token, get_password_hash
+from app.main import app as fastapi_app
 from app.models import Service, User
 
 
@@ -26,11 +30,20 @@ def createuser():
 
 
 @app.command()
-def createservice(name: str, collect: str, deploy: str):
-    rprint(f"creating service {name} {collect} {deploy}")
+def createservice(username: str, name: str, collect: str, deploy: str):
+    """Use api of application server to be able to update ServiceList on clients via websocket."""
+    rprint(f"creating service as user {username}: {name} {collect} {deploy}")
+    print("url create service: ", fastapi_app.url_path_for("create_service"))
+    access_token = create_access_token({"type": "user", "user": username}, timedelta(minutes=5))
     service = Service(name=name, collect=collect, deploy=deploy)
-    service_in_db = database.repository.add_service(service)
-    rprint(f"created service with id: {service_in_db.id}")
+    with Client(base_url="http://localhost:8000/") as client:
+        r = client.post(
+            fastapi_app.url_path_for("create_service"),
+            headers={"authorization": f"Bearer {access_token}"},
+            json=service.dict(),
+        )
+    rprint("r status code: ", r.status_code)
+    rprint(f"created service with id: {r.json()['id']}")
 
 
 @app.command()
