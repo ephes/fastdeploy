21091de1f586a665ddddc15be9cae88fd6304912
Jochen Wersdörfer <jochen@wersdoerfer.de>
2021-11-09 13:17:16 +0100

    added transistion group

--- frontend/src/components/DeploymentDetail.vue
+++ frontend/src/components/DeploymentDetail.vue
@@ -28,13 +28,28 @@ onMounted(async () => {
 
 <template>
   <div>
-    <h1>Foo!</h1>
     <h1>Deployment ID: {{ deploymentIdFromRoute }}</h1>
     <div v-if="deployment">
       <h2>deployment origin: {{ deployment.origin }}</h2>
     </div>
-    <div v-for="[id, step] in steps" :key="id" class="list-step">
-      <step :step="step" />
-    </div>
+    <transition-group name="list" tag="p">
+      <div v-for="[id, step] in steps" :key="id" class="list-step">
+        <step :step="step" />
+      </div>
+    </transition-group>
   </div>
 </template>
+<style scoped>
+.list-step {
+  margin-right: 10px;
+}
+.list-enter-active,
+.list-leave-active {
+  transition: all 1s ease;
+}
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
+}
+</style>
