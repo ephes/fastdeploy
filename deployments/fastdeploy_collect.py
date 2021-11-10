4617e6d755f19f56f73c25c917c69bec3c170216
Jochen Wersdörfer <jochen@wersdoerfer.de>
2021-11-10 17:28:12 +0100

    relative path

--- deployments/fastdeploy_collect.py
+++ deployments/fastdeploy_collect.py
@@ -6,7 +6,7 @@ from pathlib import Path
 import yaml
 
 
-playbook_path = Path("/Users/jochen/projects/fastdeploy/ansible/deploy.yml")
+playbook_path = Path(__file__).parent.parent / "ansible/deploy.yml"
 with playbook_path.open("r") as f:
     parsed = yaml.safe_load(f)
 steps = parsed[0]["tasks"]
