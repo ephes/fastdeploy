4ae5b00cf21e14ccf005cf055f94168559412ed1
Jochen Wersdörfer <jochen@wersdoerfer.de>
2021-11-22 02:24:07 +0100

    mypy

--- app/routers/deployments.py
+++ app/routers/deployments.py
@@ -1,11 +1,12 @@
 from datetime import datetime, timezone
+from typing import cast
 
 from fastapi import APIRouter, BackgroundTasks, Depends
 
 from ..auth import ServiceToken
 from ..database import repository
 from ..dependencies import get_current_active_user, get_current_service_token
-from ..models import Deployment, User
+from ..models import Deployment, Service, User
 from ..tasks import get_deploy_environment, run_deploy
 
 
@@ -25,11 +26,15 @@ async def get_deployments(current_user: User = Depends(get_current_active_user))
 async def create_deployment(
     background_tasks: BackgroundTasks, service_token: ServiceToken = Depends(get_current_service_token)
 ):
-    deployment = Deployment(
-        service_id=service_token.item_from_db.id, origin=service_token.origin, user=service_token.user
-    )
+    assert isinstance(service_token.item_from_db, Service)
+    service = cast(Service, service_token.item_from_db)
+    assert isinstance(service.id, int)
+    service_id = cast(int, service.id)
+
+    deployment = Deployment(service_id=service_id, origin=service_token.origin, user=service_token.user)
     deployment.created = datetime.now(timezone.utc)  # FIXME: use CURRENT_TIMESTAMP from database
     await repository.add_deployment(deployment)
-    environment = get_deploy_environment(service_token.item_from_db, deployment)
+
+    environment = get_deploy_environment(service, deployment)
     background_tasks.add_task(run_deploy, environment)
     return deployment
