ea5baf002d1a53e68406d0c13d2efa1cae7f32ec
Jochen Wersdörfer <jochen@wersdoerfer.de>
2021-11-22 11:43:55 +0100

    autoformat

--- frontend/tests/client.spec.ts
+++ frontend/tests/client.spec.ts
@@ -3,8 +3,8 @@ import { snakeToCamel } from "../src/client";
 describe("Test snakeToCamel function", () => {
   it("converts snake to camel case", () => {
     const testCases = [
-        [{ testCase: "foobar" }, { testCase: "foobar" }],
-        [{ test_case: "foobar" }, { testCase: "foobar" }],
+      [{ testCase: "foobar" }, { testCase: "foobar" }],
+      [{ test_case: "foobar" }, { testCase: "foobar" }],
     ];
     testCases.forEach(([input, expected]) => {
       expect(snakeToCamel(input)).toStrictEqual(expected);
