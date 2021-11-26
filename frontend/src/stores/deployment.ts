3dad08f260a03f341a8c8002ecb023c09b728fc8
Jochen Wersdörfer <jochen@wersdoerfer.de>
2021-11-26 09:58:55 +0100

    mande wip

--- frontend/src/stores/deployment.ts
+++ frontend/src/stores/deployment.ts
@@ -1,6 +1,9 @@
 import { defineStore, acceptHMRUpdate } from "pinia";
 
+import { mande } from "mande";
 import { toUtcDate } from "./datetime";
+import { useAuth } from "./auth";
+import { useSettings } from "./config";
 import { Deployment, Message, DeploymentById } from "../typings";
 
 export const useDeployments = defineStore("deployments", {
@@ -8,6 +11,17 @@ export const useDeployments = defineStore("deployments", {
     return {
       deployments: {} as DeploymentById,
     };
+  },
+  getters: {
+    mande: (state) => () => {
+      const auth = useAuth();
+      const settings = useSettings();
+      const client = mande(settings.api);
+      console.log("mande client: ", client);
+      client.options.headers.Authorization = `Bearer ${auth.accessToken}`;
+      console.log("mande client options: ", client.options);
+      return client;
+    },
   },
   actions: {
     useHMRUpdate(meta: any) {
@@ -21,10 +35,14 @@ export const useDeployments = defineStore("deployments", {
       return deployment;
     },
     async fetchDeployments() {
-        const deployments = await this.client.fetchDeployments();
-        for (const deployment of deployments) {
-          this.deployments[deployment.id] = deployment;
-        }
+      const client = this.mande();
+      console.log("mande options: ", client.options);
+      const deployments: Deployment[] = await client.get("deployments");
+      console.log("fetched deployments asdf: ", deployments);
+      // const deployments = await this.client.fetchDeployments();
+      for (const deployment of deployments) {
+        this.deployments[deployment.id] = deployment;
+      }
     },
     onMessage(message: Message) {
       if (message.type === "deployment") {
