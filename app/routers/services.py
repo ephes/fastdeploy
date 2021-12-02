efff02ed1aac43f531330fa9787722cd6be0a5e4
Jochen Wersdörfer <jochen@wersdoerfer.de>
2021-12-02 10:28:28 +0100

    added docstrings to service views..

--- app/routers/services.py
+++ app/routers/services.py
@@ -14,11 +14,18 @@ router = APIRouter(
 
 @router.get("/")
 async def get_services(_: User = Depends(get_current_active_user)) -> list[Service]:
+    """
+    Get a list of all services. Need to be authenticated.
+    """
     return await repository.get_services()
 
 
 @router.get("/names/")
 async def get_service_names(_: User = Depends(get_current_active_user)) -> set[str]:
+    """
+    Get a list of all available service names. Need to be authenticated. Only available
+    services can be created.
+    """
     return await repository.get_service_names()
 
 
@@ -27,6 +34,11 @@ async def create_service(
     service: Service,
     _: User = Depends(get_current_active_user),
 ) -> Service:
+    """
+    Create a new service. Need to be authenticated. Make sure that the service name is
+    in the list of available services and raise an error if it is not. Use the json configuration
+    from config.json to set data json field for service.
+    """
     service.id = None  # prevent client from setting the id
     service_names = await repository.get_service_names()
     if service.name not in service_names:
