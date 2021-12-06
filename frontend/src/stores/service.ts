1ddd0666377ed088b7e006e9f983d2c40bfe557a
Jochen Wersdörfer <jochen@wersdoerfer.de>
2021-12-06 17:27:46 +0100

    docstrings for service store

--- frontend/src/stores/service.ts
+++ frontend/src/stores/service.ts
@@ -2,25 +2,56 @@ import { getClient } from "./httpClient";
 import { defineStore, acceptHMRUpdate } from "pinia";
 import { Service, ServiceById, ServiceWithId, Message } from "../typings";
 
+/**
+ * This store is used to store information about services. And
+ * to add new services. And to retrieve service tokens which can
+ * be used to start deployments.
+ *
+ * The reason why we have to add services is because we need to
+ * be authenticated as a user to create a service. And we don't
+ * want to store user access tokens in the ansible vault for now.
+ */
 export const useServices = defineStore("services", {
   state: () => {
     const newService: Service = { name: "", data: {} };
     return {
+      /**
+       * Services are retrievable by id from this object.
+       */
       services: {} as ServiceById,
+      /**
+       * This is a placeholder service that is used by a user
+       * facing component to create a new service.
+       */
       new: newService,
+      /**
+       * Error message received from backend when trying to fetch
+       * a service token.
+       */
       serviceTokenErrorMessage: "",
+      /**
+       * List of available service names.
+       */
       serviceNames: [] as string[],
       client: getClient(),
     };
   },
   getters: {
+    /**
+     * Only services which are known to the backend can be added.
+     * To choose a valid service name, users have to know which ones
+     * are available. This method fetches all available names from
+     * the backend.
+     *
+     * @returns availableServiceNames {string[]} - An array of valid service names from backend
+     */
     getAvailableServiceNames: (state): string[] => {
       const availableServiceNames: string[] = [];
       const addedServiceNames = new Set(
         Object.values(state.services).map((service) => service.name)
       );
       for (const serviceName of state.serviceNames) {
-        if (!(addedServiceNames.has(serviceName))) {
+        if (!addedServiceNames.has(serviceName)) {
           availableServiceNames.push(serviceName);
         }
       }
@@ -28,11 +59,20 @@ export const useServices = defineStore("services", {
     },
   },
   actions: {
+    /**
+     * Support for HMR
+     * @param {any} meta
+     */
     useHMRUpdate(meta: any) {
       if (meta.hot) {
         meta.hot.accept(acceptHMRUpdate(useServices, meta.hot));
       }
     },
+    /**
+     * Add a service from the list of available services and add
+     * it to the backend. After that, the placeholder service is
+     * reset.
+     */
     async addService() {
       this.client
         .post<ServiceWithId>("/services/", this.new)
@@ -44,6 +84,13 @@ export const useServices = defineStore("services", {
           console.log("Error adding service", err);
         });
     },
+    /**
+     * Delete a service from the store. This is called by the event
+     * handler when a service message is received which has the deleted
+     * attribute set to true.
+     *
+     * @param service_id {number} - The id of the service to delete
+     */
     async deleteService(service_id: number) {
       this.client
         .delete<number>(`/services/${service_id}`)
@@ -54,6 +101,10 @@ export const useServices = defineStore("services", {
           console.log("delete service error: ", err);
         });
     },
+    /**
+     * Fetch all services from the backend. Used to display the list
+     * of services.
+     */
     async fetchServices() {
       const services = await (<Promise<ServiceWithId[]>>(
         this.client.get("services/")
@@ -62,11 +113,26 @@ export const useServices = defineStore("services", {
         this.services[service.id] = service;
       }
     },
+    /**
+     * Fetch all available service names from backend and store
+     * them in the store.
+     */
     async fetchServiceNames() {
       this.client.get<string[]>("services/names/").then((names) => {
         this.serviceNames = names;
       });
     },
+    /**
+     * Fetch a service token for a service. This service token then can
+     * be used to start deployments for a service. The origin can be set
+     * to generate a token for a specific origin like GitHub. Save the
+     * error message in store and return null on error.
+     *
+     * @param serviceName {string} - The name of the service for the service token
+     * @param origin {string} - The origin of the service token (frontend, github)
+     * @param expirationInDays {number} - The expiration of the service token in days (0 - 180)
+     * @returns serviceToken {string | null} - The service token or null on error
+     */
     async fetchServiceToken(
       serviceName: string,
       origin: string,
@@ -88,6 +154,13 @@ export const useServices = defineStore("services", {
         return null;
       }
     },
+    /**
+     * Event handler which is triggered by a message from the backend
+     * which has the type "service". Services can be added, updated or
+     * deleted.
+     *
+     * @param message {Message} the message which is received from the backend
+     */
     onMessage(message: Message) {
       if (message.type === "service") {
         const service = message as ServiceWithId;
