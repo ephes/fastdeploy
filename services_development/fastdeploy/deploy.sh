722bd977ecdcd9ee472d0e75d071e4902fb06726
Jochen Wersdörfer <jochen@wersdoerfer.de>
2021-12-07 07:39:14 +0100

    removed remote + regenerated config

--- services_development/fastdeploy/deploy.sh
+++ services_development/fastdeploy/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/local/bin/fish
 cd ansible
-ANSIBLE_STDOUT_CALLBACK=json_cb ~/.virtualenvs/ansible/bin/ansible-playbook deploy.yml -e '{"remote": true}'
+ANSIBLE_STDOUT_CALLBACK=json_cb ~/.virtualenvs/ansible/bin/ansible-playbook deploy.yml
 cd -
