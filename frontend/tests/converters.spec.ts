20f032b4ea6df0aecc39a796501353cffa39d4a9
Jochen Wersdörfer <jochen@wersdoerfer.de>
2021-12-07 10:59:55 +0100

    forgot test :/

--- frontend/tests/converters.spec.ts
+++ frontend/tests/converters.spec.ts
@@ -1,4 +1,5 @@
 import { snakeToCamel } from "../src/converters";
+import { utcStringToLocalDate } from "../src/converters";
 
 describe("Test snakeToCamel function", () => {
   it("converts snake to camel case", () => {
@@ -9,6 +10,22 @@ describe("Test snakeToCamel function", () => {
     ];
     testCases.forEach(([input, expected]) => {
       expect(snakeToCamel(input)).toStrictEqual(expected);
+    });
+  });
+});
+
+describe("Test utc string to local date", () => {
+  it("converts utc string to local date", () => {
+    const testCases = [
+      // note that javascript month is zero-based
+      // note that javascript date constructor takes milliseconds, python generates microseconds
+      ["2021-12-07T09:14:26.636703", new Date(2021, 11, 7, 10, 14, 26, 636)],
+    ];
+    testCases.forEach(([input, expected]) => {
+      if (typeof input === "string") {
+        const actual = utcStringToLocalDate(input);
+        expect(utcStringToLocalDate(input)).toEqual(expected);
+      }
     });
   });
 });
