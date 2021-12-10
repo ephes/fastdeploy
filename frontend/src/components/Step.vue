ecd3c2a467855b6f17196d51280faee56cdf3730
Jochen Wersdörfer <jochen@wersdoerfer.de>
2021-12-10 11:20:54 +0100

    show duration instead of created/started/finished timestamps

--- frontend/src/components/Step.vue
+++ frontend/src/components/Step.vue
@@ -1,11 +1,23 @@
 <script setup lang="ts">
 
 import { Step } from "../typings";
-import { ref, watchEffect } from "vue";
+import { ref, watchEffect, computed } from "vue";
 
 const props = defineProps<{ step: Step }>();
 const el = ref<null | HTMLElement>(null);
 
+/**
+ * Calculate step duration
+ */
+const duration = computed(() => {
+  if (props.step.finished && props.step.started) {
+    return (props.step.finished as any - (props.step.started as any)) / 1000;
+  }
+});
+
+/**
+ * Scroll to step if it's in progress
+ */
 watchEffect(() => {
   if (props.step.inProgress) {
     console.log("scroll to: ", props.step.id, el.value);
@@ -20,11 +32,7 @@ watchEffect(() => {
   <p ref="el" id="step">
     {{ step.deploymentId }} {{ step.id }} {{ step.name }}
     <br />
-    created: {{ step.created.toLocaleString() }}
-    <br />
-    started: {{ step.started }}
-    <br />
-    finished: {{ step.finished }}
+    duration: {{ duration }}
     <br />
     in progress: {{ step.inProgress }}
     <br />
