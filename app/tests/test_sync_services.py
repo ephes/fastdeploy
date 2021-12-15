ffa6b9f175c578eb802c997bfd5cc6a7154c1347
Jochen Wersdörfer <jochen@wersdoerfer.de>
2021-12-15 15:13:33 +0100

    more testcases

--- app/tests/test_sync_services.py
+++ app/tests/test_sync_services.py
@@ -9,3 +9,25 @@ def test_sync_services_add(repository):
     updated_services, deleted_services = repository.sync_services(services_from_fs, services_from_db)
     assert updated_services == services_from_fs
     assert deleted_services == []
+
+
+def test_sync_services_update(repository):
+    services_from_db = [
+        Service(id=1, name="bar", data={"description": "bar baz foo", "steps": []}),
+    ]
+    services_from_fs = [
+        Service(name="bar", data={"description": "foobar", "steps": [1]}),
+    ]
+    updated_services, deleted_services = repository.sync_services(services_from_fs, services_from_db)
+    assert updated_services == [Service(id=1, **services_from_fs[0].dict())]
+    assert deleted_services == []
+
+
+def test_sync_services_delete(repository):
+    services_from_db = [
+        Service(name="bar", data={"description": "bar baz foo", "steps": []}),
+    ]
+    services_from_fs = []
+    updated_services, deleted_services = repository.sync_services(services_from_fs, services_from_db)
+    assert updated_services == []
+    assert deleted_services == services_from_db
