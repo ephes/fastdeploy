b14359a6d1efd816712b076716937f443ecf27c1
Jochen Wersdörfer <jochen@wersdoerfer.de>
2022-03-08 14:25:49 +0100

    no successful deployment steps -> no todo steps for service test

--- tests/unit/views_test.py
+++ tests/unit/views_test.py
@@ -24,6 +24,26 @@ async def test_get_steps_to_do_from_service_has_steps_in_data(service):
     assert [step] == steps
 
 
+async def test_get_steps_to_do_from_service_only_failure_steps(uow, service_in_db):
+    service = service_in_db
+    started = datetime.utcnow() - timedelta(days=1)
+    finished = started + timedelta(minutes=3)
+    deployment = model.Deployment(
+        service_id=service.id, origin="GitHub", user="foobar", started=started, finished=finished
+    )
+    step = model.Step(name="step from database", started=started, finished=finished, state="failure")
+    async with uow:
+        await uow.deployments.add(deployment)
+        await uow.commit()
+        step.deployment_id = deployment.id
+        await uow.steps.add(step)
+        await uow.commit()
+
+    steps = await views.get_steps_to_do_from_service(service, uow=uow)
+    assert len(steps) == 1
+    assert steps[0].name == "Unknown step"
+
+
 async def test_get_steps_to_do_from_service_steps_from_last_deployment(uow, service_in_db):
     service = service_in_db
     started = datetime.utcnow() - timedelta(days=1)
