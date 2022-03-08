951581865980f2cc4df7d76b75df06ba340a5fcb
Jochen Wersdörfer <jochen@wersdoerfer.de>
2022-03-08 20:15:24 +0100

    get_deployment_details endpoint 403 for wrong service token

--- tests/e2e/deployments_api_test.py
+++ tests/e2e/deployments_api_test.py
@@ -35,10 +35,30 @@ async def test_get_deployments_happy(app, deployment_in_db, valid_access_token_i
 # test get_deployment_details endpoint
 
 
-async def test_get_deployment_details_no_such_deployment(app, base_url, valid_service_token_in_db):
+async def test_get_deployment_details_no_such_deployment(app, valid_service_token_in_db):
     headers = {"authorization": f"Bearer {valid_service_token_in_db}"}
-    async with AsyncClient(app=app, base_url=base_url) as client:
+    async with AsyncClient(app=app, base_url="http://test") as client:
         response = await client.get(app.url_path_for("get_deployment_details", deployment_id=666), headers=headers)
 
     assert response.status_code == 404
     assert response.json() == {"detail": "Deployment not found"}
+
+
+async def test_get_deployment_details_wrong_service(app, uow, valid_service_token_in_db):
+    service = model.Service(name="another service")
+    async with uow:
+        await uow.services.add(service)
+        await uow.commit()
+        assert isinstance(service.id, int)
+        deployment = model.Deployment(service_id=service.id, origin="frontend", user="asdf")
+        await uow.deployments.add(deployment)
+        await uow.commit()
+
+    headers = {"authorization": f"Bearer {valid_service_token_in_db}"}
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        response = await client.get(
+            app.url_path_for("get_deployment_details", deployment_id=deployment.id), headers=headers
+        )
+
+    assert response.status_code == 403
+    assert response.json() == {"detail": "Wrong service token"}
