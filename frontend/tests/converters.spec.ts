f7e256ae989ba4cacbedc95e4bf85305c0ed9f11
Jochen Wersdörfer <jochen@wersdoerfer.de>
2022-03-13 11:01:18 +0100

    fixed utc fuckup tests

--- frontend/tests/converters.spec.ts
+++ frontend/tests/converters.spec.ts
@@ -22,7 +22,7 @@ describe("Test utc string to local date", () => {
     const testCases = [
       // note that javascript month is zero-based
       // note that javascript date constructor takes milliseconds, python generates microseconds
-      ["2021-12-07T09:14:26.636703", new Date(2021, 11, 7, 10, 14, 26, 636)],
+      ["2021-12-07T09:14:26.636703", new Date(2021, 11, 7, 9, 14, 26, 636)],
       [null, null],
     ];
     testCases.forEach(([input, expected]) => {
@@ -37,19 +37,19 @@ describe("Test utc string to local date", () => {
       [
         { foo: "2021-12-07T09:14:26.636703" },
         ["foo"],
-        { foo: new Date(2021, 11, 7, 10, 14, 26, 636) },
+        { foo: new Date(2021, 11, 7, 9, 14, 26, 636) },
       ],
       // nonexisting key should be fine
       [
         { foo: "2021-12-07T09:14:26.636703", bar: "" },
         ["foo", "baz"],
-        { foo: new Date(2021, 11, 7, 10, 14, 26, 636), bar: "" },
+        { foo: new Date(2021, 11, 7, 9, 14, 26, 636), bar: "" },
       ],
       // copy all keys
       [
         { foo: "2021-12-07T09:14:26.636703", bar: "asdf" },
         ["foo"],
-        { foo: new Date(2021, 11, 7, 10, 14, 26, 636), bar: "asdf" },
+        { foo: new Date(2021, 11, 7, 9, 14, 26, 636), bar: "asdf" },
       ],
     ];
     testCases.forEach(([input, keys, expected]) => {
