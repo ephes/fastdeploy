64b6a5534eefc140c4ec322e7aa6d179adc3c807
Jochen Wersdörfer <jochen@wersdoerfer.de>
2022-03-16 23:10:56 +0100

    make sure steps are parsed from yaml

--- tests/unit/filesystem_test.py
+++ tests/unit/filesystem_test.py
@@ -1,3 +1,5 @@
+import json
+
 import pytest
 
 
@@ -15,3 +17,17 @@ def test_config_has_to_be_dict(services_filesystem):
         config_file.write("[]")
     with pytest.raises(TypeError):
         services_filesystem.get_config_by_name("service")
+
+
+def test_get_steps_from_playbook(services_filesystem):
+    service_path = services_filesystem.root / "service"
+    service_path.mkdir()
+    playbook_path = service_path / "playbook.yml"
+    with playbook_path.open("w") as playbook_file:
+        playbook_file.write("- hosts: localhost\n" "  tasks:\n" "    - name: task1\n")
+    config_path = service_path / "config.json"
+    with config_path.open("w") as config_file:
+        config = {"ansible_playbook": str(playbook_path)}
+        config_file.write(json.dumps(config))
+    config = services_filesystem.get_config_by_name("service")
+    assert config["steps"] == [{"name": "Gathering Facts"}, {"name": "task1"}]
