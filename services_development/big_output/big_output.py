cb133a0ebc2f9999c6f31196e38ffb160a53d2fc
Jochen Wersdörfer <jochen@wersdoerfer.de>
2022-03-18 11:12:59 +0100

    fail on second step

--- services_development/big_output/big_output.py
+++ services_development/big_output/big_output.py
@@ -5,6 +5,8 @@ from time import sleep
 
 
 for i in range(2):
-    message = {"name": f"message {i}", "state": "success", "foo": "x" * 100000}
+    message = {"name": f"message {i}", "state": "success"}
+    if i == 1:
+        message["foo"] = "y" * 100000
     print(json.dumps(message), flush=True)
     sleep(0.5)
